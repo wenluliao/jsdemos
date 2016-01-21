c9cbed333853cb482af48b7ada1536b00812d912
wenluliao <wenluliao@hotmail.com>
2016-01-21 20:03:47 +0800

    二维二级表控件初步完成

--- erweiTable/js/erweitable.js
+++ erweiTable/js/erweitable.js
@@ -198,13 +198,3 @@ function erWeiErJiTable() {
 		return obj.find(".td_"+xindex+"_"+yindex);
 	}
 }
-
-$(function(){
-	var ewejtable = new erWeiErJiTable();
-	ewejtable.addColumnHead("sex","性别",[{'男':'=1'},{'女':'=0'}]);
-	ewejtable.addRowsHead("age","年龄",[{'小于30':'<30'},{'30~40':['>=30','<40']}])
-	$("body").html(ewejtable.getHtmlStr());
-//	console.log(ewejtable.getHtmlStr());
-})
-
-
